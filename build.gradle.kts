943d2388ac3e855887c33ea29df8025a1f67ea12
Neric Wizard <njeckneric507@gmail.com>
2025-11-11 16:52:46 +0100

    Clean and safe initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id 'com.android.application'
+    id 'com.android.library'
+}
+
+android {
+}
+
+dependencies {
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+}
